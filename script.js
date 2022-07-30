eb85644261e58f704e4cbdeebab8f50538e3d6a6
Kamila Jesus <kamilaalves727@gmail.com>
2022-07-29 21:54:02 -0300

    new commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var consultaCEP = fetch(' https://viacep.com.br/ws/01001000/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if(r.erro){
+          throw Error("esse cep não existe!")
+        }else
+          console.log(r)
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('processamento concluido'));
+
+console.log(consultaCEP);
